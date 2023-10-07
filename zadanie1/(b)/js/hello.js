dbe8c2e0b8fa989b3fd167a89629c36be7e58f3c
Елизавета <74406940+krrrrrlii@users.noreply.github.com>
2023-10-07 15:22:53 +0300

    Update hello.js

--- zadanie1/(b)/js/hello.js
+++ zadanie1/(b)/js/hello.js
@@ -1,28 +1,28 @@
-function calculateEntropy() {
-    var inputText = document.getElementById("text").value;
-    var entropy = getShannonEntropy(inputText);
-    document.getElementById("entropy").innerHTML = "Entropy: " + entropy.toFixed(2);
+const calculateEntropy = () => {
+const inputText = document.getElementById("text").value;
+const entropy = getShannonEntropy(inputText);
+document.getElementById("entropy").innerHTML = "Entropy: " + entropy.toFixed(2);
 }
 
-function getShannonEntropy(text) {
-    var individualChars = text.split("");
-    var charCounts = {};
-    var entropy = 0;
+const getShannonEntropy = (text) => {
+const individualChars = text.split("");
+const charCounts = {};
+let entropy = 0;
 
-    for (var i = 0; i < individualChars.length; i++) {
-        var char = individualChars[i];
+for (let i = 0; i < individualChars.length; i++) {
+    const char = individualChars[i];
 
-        if (charCounts[char] === undefined) {
-            charCounts[char] = 1;
-        } else {
-            charCounts[char]++;
-        }
+    if (charCounts[char] === undefined) {
+        charCounts[char] = 1;
+    } else {
+        charCounts[char]++;
     }
+}
 
-    Object.keys(charCounts).forEach(function (char) {
-        var frequency = charCounts[char] / individualChars.length;
-        entropy -= frequency * (Math.log2(frequency));
-    });
+Object.keys(charCounts).forEach((char) => {
+    const frequency = charCounts[char] / individualChars.length;
+    entropy -= frequency * (Math.log2(frequency));
+});
 
-    return entropy;
+return entropy;
 }
